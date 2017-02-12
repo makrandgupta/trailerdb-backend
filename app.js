f9340d3de4de1e6ddd8a481f93ed0046a6fe894c
Makrand Gupta <makrand1996@gmail.com>
2017-02-12 07:33:50 -0500

    CORS + expose headers

--- app.js
+++ app.js
@@ -30,9 +30,57 @@ function createServer (logger) {
   if (logger) settings.log = logger;
 
   var server = restify.createServer(settings);
-
+  
+  // restify.CORS.ALLOW_HEADERS.push('authorization');
   server.use(restify.acceptParser(server.acceptable));
   server.use(restify.queryParser());
+  server.use(restify.CORS({
+      headers: [
+          'authorization',
+          'x-total-count',
+          'link'
+      ]
+  }));
+  restify.CORS.ALLOW_HEADERS.push( "authorization");
+  restify.CORS.ALLOW_HEADERS.push( "withcredentials");
+  restify.CORS.ALLOW_HEADERS.push( "x-requested-with");
+  restify.CORS.ALLOW_HEADERS.push( "x-forwarded-for");
+  restify.CORS.ALLOW_HEADERS.push( "x-real-ip");
+  restify.CORS.ALLOW_HEADERS.push( "x-customheader");
+  restify.CORS.ALLOW_HEADERS.push( "user-agent");
+  restify.CORS.ALLOW_HEADERS.push( "keep-alive");
+  restify.CORS.ALLOW_HEADERS.push( "host");
+  restify.CORS.ALLOW_HEADERS.push( "accept");
+  restify.CORS.ALLOW_HEADERS.push( "connection");
+  restify.CORS.ALLOW_HEADERS.push( "upgrade"              );
+  restify.CORS.ALLOW_HEADERS.push( "content-type");
+  restify.CORS.ALLOW_HEADERS.push( "dnt"); // Do not track
+  restify.CORS.ALLOW_HEADERS.push( "if-modified-since");
+  restify.CORS.ALLOW_HEADERS.push( "cache-control");
+
+  // Manually implement the method not allowed handler to fix failing preflights
+  //
+  server.on( "MethodNotAllowed", function( request, response )
+  {
+      if ( request.method.toUpperCase() === "OPTIONS" )
+      {
+          // Send the CORS headers
+          //
+          response.header( "Access-Control-Allow-Credentials", true                                    );
+          response.header( "Access-Control-Allow-Headers",     restify.CORS.ALLOW_HEADERS.join( ", " ) );
+          response.header( "Access-Control-Allow-Methods",     "GET, POST, PUT, DELETE, OPTIONS"       );
+          response.header( "Access-Control-Allow-Origin",      request.headers.origin                  );
+          response.header( "Access-Control-Max-Age",           0                                       );
+          response.header( "Content-type",                     "text/plain charset=UTF-8"              );
+          response.header( "Content-length",                   0                                       );
+
+          response.send( 204 );
+      }
+      else
+      {
+          response.send( new restify.MethodNotAllowedError() );
+      }
+  } );
   server.use(restify.bodyParser({
     mapParams: true,
     mapFiles: false,
